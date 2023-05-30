6da80ad3c6000fdb3954632e65e6977e08baa9e2
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-05-29 18:46:20 -0600

    Mejoras de legibilidad y esqueleto de news scrapper

---
+++ common.py
@@ -0,0 +1,11 @@
+import yaml                                                               # Importamos la librería "yaml" en virtual env (conda install yaml)
+
+__config = None                                                           # Variable global, nos va a servir para poder "cachear" nuestra configuración
+
+def config():
+  global __config
+  if not __config:                                                        # Solo vamos a leer la configuracion una sola vez, y si ya esta cargada solo la devolvemos y si no esta cargada la devolvemos
+    with open('config.yaml', mode='r') as file:                           # Abrimos el archivo config.yaml
+      __config = yaml.load(file, Loader=yaml.FullLoader)                  # Guardamos el archivo a "__config", funcion retorna un diccionario
+  #   __config = yaml.safe_load(file)
+  return __config                                                         # Retornamos nuestra configuracion
--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import argparse
 import logging
 import news_page_object as news
-from common import config  # Importamos la funcion "config" del modulo "common.py"
-logging.basicConfig(level=logging.INFO)
+from common import config                                                   # Importamos la funcion "config" del modulo "common.py"
+
+logging.basicConfig(level=logging.INFO)                                     # Configuramos la manera de hacer logging y mostrarlo en pantalla al cambiarle el "level"
+logger = logging.getLogger(__name__)                                        # Obtenemos una referencia a nuestro "logger" y le damos el nombre de nuestro archivo
 
 
+def _news_scraper(news_site_uid):                                           # Ejecuta el "scrapper" recibe como argumento el "news_site_uid"
+  host = config()['news_sites'][news_site_uid]['url']                       # Obtenemos el "host", le pasamos el id que el usuario selecciono y solicitamos la llave "url"
+  logging.info('Beginning scraper for {}'.format(host))                     # Mostramos la información del "logging" con el nombre del "host"
+    
 
+if __name__ == '__main__':                                                  # Entry Point
+  parser = argparse.ArgumentParser()                                        # Instanciamos un objeto de la clase ArgfParser
 
-logger = logging.getLogger(__name__)
-
-def _news_scraper(news_site_uid):
-    host = config()['news_sites'][news_site_uid]['url']
-
-    logging.info('Beginning scraper for {}'.format(host))
-    homepage = news.HomePage(news_site_uid, host)
-
-    for link in homepage.article_links:
-        print(link)
-
-
-if __name__ == '__main__':   # Entry Point
-    parser = argparse.ArgumentParser() # Instanciamos un objetod de la clase ArgumentoParser
-
-    news_site_choices = list(config()['news_sites'].keys()) # Nos regresa un iterador lo convertimos en una lista
-    parser.add_argument('news_site',                             # Añadimos varias opciones
-                        help='The news site that you want to scrape',
-                        type=str,
-                        choices=news_site_choices) # Las opciones que estan en "config.yaml" 
-    args = parser.parse_args()
-    _news_scraper(args.news_site)     
+  news_site_choices = list(config()['news_sites'].keys())                   # Nos regresa un iterador lo convertimos en una lista con los "keys" de "news_site_choices"
+  parser.add_argument('news_site',                                          # Añadimos varias opciones
+                      help='The news site that you want to scrape',
+                      type=str,
+                      choices=news_site_choices)                            # Las opciones que estan en "config.yaml" en el mapa "news_site_choices" que convertimos a lista arriba
+  args = parser.parse_args()                                                # Le pedimos al "parser" que los parsee y nos de un objeto con ellos
+  _news_scraper(args.news_site)                                             # Comenzamos nuestro "scrapper"
