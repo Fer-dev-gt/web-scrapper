09894a6e172e6521776b8021f4b7568113bf3235
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-06-01 17:52:12 -0600

    Manejamos a todas las noticias que tengan NaN como valor es la columna 'title' y las rellenamos con el titulo de la URL

--- newspaper_receipe_automation.py
+++ newspaper_receipe_automation.py
@@ -8,40 +8,54 @@ logger = logging.getLogger(__name__)                                            
 
 
 def main(filename):                                                                   # Ejecuta las funciones que limpian y transforman a nuestro DataFrame el cual retornamos, Recibe como argumento al "filename" que habiemos creado como argumento en el Entry Point
-  logger.info('Starting cleaning process')
+  logger.info('Starting cleaning process 🧹')
   df = _read_data(filename)                                                           # Leemos los datos y creamos un DataFrame con este función, 'df' significa DataFrame
   newspaper_uid = _extract_newspaper_uid(filename)                                    # Guardamos y extraemos el UID (nombre del periodico) de los articulos de noticias, pasamos el archivo CSV
   df = _add_newspaper_uid_column(df, newspaper_uid)                                   # Modificamos el DataFrame en las columnas le añadimos el UID, recibe 2 parámetros, el DataFrama "df" como lo llevamos trabajando y el valor del UID al cual lo meteremos en el DataFrame
   df = _extract_host(df)                                                              # Extraemos el 'host' del DataFrame (Ej: elpais.com) y lo agregamos como nueva columnas, enviamos el DataFrame
+  df = _fill_missing_titles(df)                                                       # Llenamos los "missign titles" de las noticias, estos aparecen como NaN y extraemos el titulo de la URL y lo colocamos en el DataFrame
   return df                                                                           # Regresamos el DataFrame "Transformado" al Entry Point
 
 
 
 def _read_data(filename):                                                             # Lee el archivo CSV y lo devuelve como DataFrame
-  logger.info('Reading file {}'.format(filename))
+  logger.info('Reading file {} 🤓'.format(filename))
   return pd.read_csv(filename)                                                        # Retornamos el resultado (DataFrame) de leer el CSV usando la libreria Pandas como "pd"
 
 
 
 def _extract_newspaper_uid(filename):                                                 # Retorna el UID de la noticia (nombre del periodico) dentro del CSV
-  logger.info('Extracting newspaper uid')
+  logger.info('Extracting newspaper uid 💉')
   newspaper_uid = filename.split('_')[0]                                              # Obtenemos el UID del nombre de nuestro archivo (Le dimos el formato que comenzara el con su nombre Ej, "elpais_something") y lo obtenemos con el método ".split('_')"
-  logger.info('Newspaper uid detected: {}'.format(newspaper_uid))
+  logger.info('Newspaper uid detected: {} 🕵🏽‍♂️'.format(newspaper_uid))
   return newspaper_uid                                                                # Retornamos el UID (el nombre del periodico)
 
 
 
 def _add_newspaper_uid_column(df, newspaper_uid):                                     # Agrega una columna al DataFrame con el valor del UID del periodico          
-  logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
+  logger.info('Filling newspaper_uid column with {} ⛽️'.format(newspaper_uid))
   df['newspaper_uid'] = newspaper_uid                                                 # Creamos una columna llamada 'newspaper_uid' y le agregamos el valor que obtuvimos para que lo podamos aplicar a cualquier DataSet
   return df                                                                           # Retornamos el DataFrame modificado
 
 
 
 def _extract_host(df):                                                                # Extrae el Host de las URLs y crea una columna llamada 'host' y llena los valores de sus celdas
-  logging.info('Extracting host form urls')
+  logger.info('Extracting host form urls 👽')
   df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)                      # Creamos la columna 'host' y los valores los obtenemos usando el método ".apply" que saca de la columna 'url' el nombre del host, usa como parámetro una función lambda que usa la 'url' para parsear el 'host' con el método 'urlparse(url).netloc', la propiedad "netloc" es en realidad el 'host
   return df                                                                           # retornamos el DataFrame modificado
+
+
+
+def _fill_missing_titles(df):                                                         # Extraemos el titulo de la URL y lo colocamos en el DataFrame a las noticias que no tienen titulo
+  logger.info('Filling missing titles 🗂️')
+  missing_titles_mask = df['title'].isna()                                            # Generamos nuestra mascara Booleana para identificar donde estan los datos faltantes seleccionando la columna 'title' y usando el método ".isna()"        
+  missig_titles = (df[missing_titles_mask]['url']                                     # Extramos el titulo de la columna 'url' con los valores que faltan guaradados en 'missing_title_mask' y le aplicamos varias transformaciones para que tenga el formato adecuado
+                  .str.extract(r'(?P<missing_titles>[^/]+)$')                         # Usando Expresiones Regulares (le decimos que selecciones todo menos las diagonales, queremos que haya uno o más y nos vamos hasta el final de nuestro string)y dandole un nombre al grupo de coincidencias "missing_titles" usamos el método ".str.extract()"
+                  .applymap(lambda title: title.replace('-', ' '))                    # Reemplazamos las diagonales que viened de la URL y los reemplazamos por espacios usando el método de Pandas ".applymap()"
+                  .applymap(lambda final_title: final_title.capitalize()))            # Por ultimo hacemos que las noticias empiezen con mayuscula para que tenga el mismo formato que el resto de noticias
+  df.loc[missing_titles_mask, 'title'] = missig_titles.loc[:, 'missing_titles']       # Asignamos los titulos extraidos a una columna llamada usando la notación ".loc[]", le decimos que seleccionas todas las filas donde hay 'missing titles' y queremos la columna 'title'. Despues del '=' le pasamos todos nuestros 'missing_titles' con ':' y le pasamos la columna 'missing_titles' que es el nombre que le dimos a nuestro grupo en las Expresiones Regulares
+  return df                                                                           # Regresamos nuestro DataFrame actualizado
+                  
 
 
 
