dc58084cce932b4e59fc3fe6b7c326015064be24
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-06-05 19:48:45 -0600

    Addition of commentary and improves of readability

--- load/article.py
+++ load/article.py
@@ -1,17 +1,18 @@
-from sqlalchemy import Column, String, Integer
-from base import Base
+from sqlalchemy import Column, String, Integer                                # Nos traemos de la libreria 'sqlalchemy' la 'Column, String e Integer'
+from base import Base                                                         # Del archivo 'base.py' nos traemos nuestra Clase 'Base'
 
-class Article(Base):
-  __tablename__ = 'articles'
-  id = Column(String, primary_key=True)
-  body = Column(String)
+class Article(Base):                                                          # Declaramos nuestra clase Article que extiende de la clase Base
+  __tablename__ = 'articles'                                                  # Le decimos como se va a llamar nuestra tabla
+  id = Column(String, primary_key=True)                                       # Declaramos nuestra estructura de datos, todos los datos que recolectamos en nuestro CSV los estamos esctructurando para nuestra Base de Datos
+  body = Column(String)                                                       # La mayoria de los datos con los que trabajamos son de tipo String
   host = Column(String)
   title = Column(String)
   newspaper_uid = Column(String)
-  n_tokens_body = Column(Integer)
-  n_tokens_title = Column(Integer)
+  n_tokens_body = Column(Integer)                                             # Pero tambien podemos usar tipo Integer entre otros
+  n_tokens_title = Column(Integer)                                            # Le pusimos tipo Integer a los tokens porque tienen que ser números enteros, no podemos tener medio token (media palabra)
   url = Column(String, unique=True)
 
+  # Generamos nuestra inicialización del Objeto, nos traemos las variables de arriba y se las asiganmos a nuestras variables de instancia
   def __init__(self, uid, body, host, title, newspaper_uid, n_tokens_body, n_tokens_title, url): 
     self.id = uid
     self.body = body
--- load/main.py
+++ load/main.py
@@ -1,21 +1,22 @@
-import argparse
+# Archivo principal que se encarga de subir nuestros datos a la DB SQL
+import argparse                                                                         # Importamos 'argparse' porque queremos convertir nuestro script en un programa ejecutable
 import pandas as pd
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from article import Article
-from base import Base, engine, Session
+from article import Article                                                             # Nos traemos del archivo 'article.py' la clase Article que extiende de la clase Base              
+from base import Base, engine, Session                                                  # Nos traemos las variable que declaramos en el archivo 'base'
 
-def main(filename):
-  Base.metadata.create_all(engine)
-  session = Session()
-  articles = pd.read_csv(filename, on_bad_lines='skip', sep=';')
+def main(filename):                                                                     # La función main recibe el 'filename'
+  Base.metadata.create_all(engine)                                                      # Esto nos permite generar nuestra 'squema' en nuestra base de datos
+  session = Session()                                                                   # Inicializamos nuestra sesion
+  articles = pd.read_csv(filename, on_bad_lines='skip', sep=';')                        # Vamos a leer nuestros articulos (filename) usando Pandas
 
-  for index, row in articles.iterrows():
+  for index, row in articles.iterrows():                                                # Usamos el método de Pandas 'iterrows()' que nos permite generar un 'loop' adentro de cada una de nuestras filas de nuestro DataFrame que nos data un "index, row"
     logger.info('Loading article uid {} into DB'.format(row['uid']))
-    article = Article(row['uid'],
+    article = Article(row['uid'],                                                       # Generamos nuestro articulos al pasarle nuestros valores al constructor de la Clase Article, siempre usando la notación 'row['value']'
                       row['body'],
                       row['host'],
                       row['newspaper_uid'],
@@ -23,16 +24,16 @@ def main(filename):
                       row['n_tokens_title'],
                       row['title'],
                       row['url'])
-    session.add(article)
+    session.add(article)                                                                # Esta instucción ya nos ingresa nuestros articulos a la base de datos
 
-  session.commit()
-  session.close()
+  session.commit()                                                                      # Le damos un commit a la sesion
+  session.close()                                                                       # Cerramos la sesion
 
 
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('filename', help='The file you want to load into the db', type=str)
-  args = parser.parse_args()
-  main(args.filename)
+  parser = argparse.ArgumentParser()                                                        # Inicializamos nuestro parser usando el método ArgumentParser()
+  parser.add_argument('filename', help='The file you want to load into the db', type=str)   # Le añadimos un argumento al que llamaremos 'filename' y le damos un string de 'help=' y le decimos que es de tipo str
+  args = parser.parse_args()                                                                # 'Parseamos' nuestro argumentos  
+  main(args.filename)                                                                       # Ejecutamos nuestra función main con los argumento parseados con nombre filename
